89669320d3e4df1ccf4f24f562eac517db4a5071
Fortis <luxzoy67@gmail.com>
2026-04-15 22:23:02 +0900

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font_with_fallback({
+    "JetBrains Mono",
+    "SF Mono"
+  }),
+
+  font_size = 12.5,
+
+  window_background_opacity = 0.7,
+  macos_window_background_blur = 35,
+
+  colors = {
+    foreground = "#d1ba8e",
+    background = "#231e3b",
+  },
+
+  enable_tab_bar = true,
+  window_decorations = "RESIZE",
+  enable_scroll_bar = false,
+}
